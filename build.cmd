d5ed05dd4e0ad3bc13dc47d3bef4e1c5a443a356
Artem Kuznetsov <arkuznetsov@gmail.com>
2022-09-08 16:18:09 +0300

    fix: Доработан скрипт сборки

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set v8version=8.3.14.1779
+set v8version=8.3.18.1741
 set v8path="C:\Program Files\1cv8\%v8version%\bin\1cv8"
 set edtprojectpath=%cd%\dp
 set modulesprojectpath=%cd%\modules\SerLib1C\dp
@@ -16,7 +16,7 @@ echo Starting export to 1C:Designer XML format...
 
 call ring edt workspace export --project "%edtprojectpath%" --configuration-files "%tmpdir%\main" --workspace-location "%tmpdir%\ws"
 
-rm -fr "%tmpdir%\ws"
+rmdir /S /Q "%tmpdir%\ws"
 md "%tmpdir%\ws"
 
 call ring edt workspace export --project "%modulesprojectpath%" --configuration-files "%tmpdir%\modules" --workspace-location "%tmpdir%\ws"
@@ -26,17 +26,17 @@ echo Starting build external data processors ^& reports...
 FOR /f %%f IN ('dir /b /a-d "%tmpdir%\main\ExternalDataProcessors\*.xml"') DO (
     FOR %%i IN (%%~nf) DO (
         echo Building %%~ni...
-        %v8path% DESIGNER /LoadExternalDataProcessorOrReportFromFiles "%tmpdir%\main\ExternalDataProcessors\%%~ni.xml" "%buildpath%"
+        %v8path% DESIGNER /LoadExternalDataProcessorOrReportFromFiles "%tmpdir%\main\ExternalDataProcessors\%%~ni.xml" "%buildpath%" /DisableStartupDialogs
     )
 )
 
 FOR /f %%f IN ('dir /b /a-d "%tmpdir%\modules\ExternalDataProcessors\*.xml"') DO (
     FOR %%i IN (%%~nf) DO (
         echo Building %%~ni...
-        %v8path% DESIGNER /LoadExternalDataProcessorOrReportFromFiles "%tmpdir%\modules\ExternalDataProcessors\%%~ni.xml" "%buildpath%"
+        %v8path% DESIGNER /LoadExternalDataProcessorOrReportFromFiles "%tmpdir%\modules\ExternalDataProcessors\%%~ni.xml" "%buildpath%" /DisableStartupDialogs
     )
 )
 
-rm -fr "%tmpdir%"
+rmdir /S /Q "%tmpdir%"
 
 echo Finish building external data processors ^& reports.
